9e85e6b91d90cd66b8d72096601f07d5ed322f39
SaiRoopa-0904 <sairoopa@scube.me>
2025-07-19 16:02:48 +0530

    cors added to server

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-
-
-
 const jsonServer = require("json-server");
 const server = jsonServer.create();
+const cors = require("cors");
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const routes = require("./routes.json");
 const rewriter = jsonServer.rewriter(routes);
 
+
+server.use(cors());
 server.use(middlewares);
 server.use(rewriter);
 server.use(router);
 
-const PORT = 3001;
+
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-  console.log(`JSON Server running at http://localhost:${PORT}`);
+  console.log(`✅ JSON Server running at http://localhost:${PORT}`);
 });
